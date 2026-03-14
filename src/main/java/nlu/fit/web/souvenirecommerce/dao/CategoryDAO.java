09c63bf7dde24d8091ab3853063c9fbdeff20d8c
Thang-23130300 <23130300@st.hcmuaf.edu.vn>
2026-03-14 19:03:12 +0700

    feat(DAO) : add ProductDAO and CategoryDAO using jdbi

---
+++ src/main/java/nlu/fit/web/souvenirecommerce/dao/CategoryDAO.java
@@ -0,0 +1,66 @@
+package nlu.fit.web.souvenirecommerce.dao;
+
+import nlu.fit.web.souvenirecommerce.model.Category;
+import nlu.fit.web.souvenirecommerce.util.JdbiFactory;
+import org.jdbi.v3.core.Jdbi;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CategoryDAO {
+    private Jdbi jdbi;
+
+    public CategoryDAO() throws IOException {
+        this.jdbi = JdbiFactory.getJdbi();
+    }
+    //SQL
+    private static final String SELECT_ALL = """
+        SELECT id, category_name AS name, image
+        FROM categories
+        ORDER BY id DESC
+    """;
+    private static final String SELECT_BY_ID = """
+        SELECT id, category_name AS name, image
+        FROM categories
+        WHERE id = :id
+    """;
+    private static final String SELECT_TOP_SELLING = """
+        SELECT c.id, c.category_name AS name, c.image
+        FROM categories c
+        JOIN products p ON c.id = p.category_id
+        GROUP BY c.id, c.category_name, c.image
+        ORDER BY SUM(p.total_sold) DESC LIMIT :limit
+    """;
+    private static final String SELECT_TOP_SELLING_IDS = """
+        SELECT category_id
+        FROM products
+        GROUP BY category_id
+        ORDER BY SUM(total_sold) DESC LIMIT :limit
+    """;
+    private static final String SELECT_NOTIN = """
+        SELECT id, category_name AS name, image
+        FROM categories
+        WHERE id NOT IN (<ids>)
+    """;
+    // method
+    public List<Category> getAlCategories(){
+        return jdbi.withHandle(handle -> handle.createQuery(SELECT_ALL).mapToBean(Category.class).list());
+    }
+    public Category getCategoryById(int id){
+        return jdbi.withHandle(handle -> handle.createQuery(SELECT_BY_ID).bind("id",id).mapToBean(Category.class).findOne().orElse(null));
+    }
+    public List<Category> getTopSellingCategories(int limit){
+        return jdbi.withHandle(handle -> handle.createQuery(SELECT_TOP_SELLING).bind("limit",limit).mapToBean(Category.class).list());
+    }
+    public List<Integer> getTopSellingCategoriesIds(int limit){
+        return jdbi.withHandle(handle -> handle.createQuery(SELECT_TOP_SELLING_IDS).bind("limit",limit).mapTo(Integer.class).list());
+    }
+    public List<Category> getCategoriesNotIn(List<Integer> usedIds){
+        if(usedIds == null || usedIds.isEmpty()){
+            return getAlCategories();
+        }
+        return jdbi.withHandle(handle -> handle.createQuery(SELECT_NOTIN).bindList("ids",usedIds).mapToBean(Category.class).list());
+    }
+
+
+}
---
+++ src/main/java/nlu/fit/web/souvenirecommerce/dao/ProductDAO.java
@@ -0,0 +1,54 @@
+package nlu.fit.web.souvenirecommerce.dao;
+
+import nlu.fit.web.souvenirecommerce.model.Product;
+import nlu.fit.web.souvenirecommerce.util.JdbiFactory;
+import org.jdbi.v3.core.Jdbi;
+
+import java.io.IOException;
+
+import java.util.List;
+
+
+public class ProductDAO {
+    // SQL
+    private static final String BASE_SELECT = """
+            SELECT  p.id, p.category_id, p.name, p.description, p.original_price, p.image_url, p.stock_quantity, p.total_sold,
+                    COALESCE(ROUND(AVG(r.rating), 1), 0) AS avg_rating,
+                    COUNT(r.id) AS review_count,
+                    CASE
+                    WHEN MAX(pr.discount_percent) IS NOT NULL
+                    THEN ROUND(p.original_price * (100 - MAX(pr.discount_percent)) / 100, 2)
+                    ELSE NULL
+                    END AS sale_price
+            FROM products p
+            LEFT JOIN reviews r ON p.id = r.product_id
+            LEFT JOIN promotions pr ON p.id = pr.product_id
+                 AND (pr.start_date IS NULL OR pr.start_date <= NOW())
+                 AND (pr.end_date IS NULL OR pr.end_date >= NOW())
+            GROUP BY p.id, p.category_id, p.name, p.description, p.original_price, p.image_url, p.stock_quantity, p.total_sold
+            """;
+    private Jdbi jdbi;
+
+    public ProductDAO() throws IOException {
+        this.jdbi = JdbiFactory.getJdbi();
+    }
+    // Home
+    // Best selling
+    public List<Product> getBestSellingProducts(int limit){
+        String sql = BASE_SELECT + " ORDER BY p.total_sold DESC LIMIT : limit";
+        return jdbi.withHandle(handle -> handle.createQuery(sql).bind("limit",limit).mapToBean(Product.class).list());
+    }
+    // Newest Product
+    public List<Product> getNewestProducts(int limit){
+        String sql = BASE_SELECT + " ORDER BY p.id DESC LIMIT : limit";
+        return jdbi.withHandle(handle -> handle.createQuery(sql).bind("limit",limit).mapToBean(Product.class).list());
+    }
+    // Top rated
+    public List<Product> getTopRatedProducts(int limit){
+        String sql = BASE_SELECT + " ORDER BY avg_rating DESC, review_count DESC LIMIT : limit";
+        return jdbi.withHandle(handle -> handle.createQuery(sql).bind("limit",limit).mapToBean(Product.class).list());
+
+    }
+
+
+}
