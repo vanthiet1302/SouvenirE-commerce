67819e1889ee6c9ea5ca3bb9f1efa0dd7333e176
Thang-23130300 <23130300@st.hcmuaf.edu.vn>
2026-03-14 12:22:56 +0700

    feat(util) : add DBContext and ApplicationLoader for DB

---
+++ src/main/java/nlu/fit/web/souvenirecommerce/util/DBContext.java
@@ -0,0 +1,21 @@
+package nlu.fit.web.souvenirecommerce.util;
+
+import nlu.fit.web.souvenirecommerce.util.ApplicationLoader;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBContext {
+    public static Connection getConnection() throws IOException, SQLException, ClassNotFoundException {
+        Properties prop = ApplicationLoader.load();
+        String dbUrl = prop.getProperty("db.url");
+        String dbUser = prop.getProperty("db.user");
+        String dbPassword = prop.getProperty("db.password");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+}
